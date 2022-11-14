394e7d98483af8c5e308087fe9cbe76dce1d0e4c
Quang Nguyen <quangtn001@gmail.com>
2022-11-14 15:52:52 +0700

    update 14/11/2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
-const search = document.querySelector('.search-box');
+let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    menu.classList.remove('active');
+};
+
+let menu = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+    search.classList.remove('active')
+};
+// Hide menu and search box when scroll
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active')
 };
